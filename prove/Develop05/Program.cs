fc9a28b0f5cb6da90bf828a4df10412e0ceab6de
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-03 19:53:28 +0100

    Week 05 Develop: Eternal Quest Program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public enum GoalType
+{
+    Simple,
+    Eternal,
+    Checklist
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        GoalManager manager = new GoalManager();
+        manager.Start();
     }
 }
