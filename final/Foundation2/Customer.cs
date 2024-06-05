d3eb0e87d90f62223e2ac1c33061c0b918baa829
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 15:34:07 +0100

    Create Customer.cs
    Design Activity

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,16 @@
+class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string Name, Address Address)
+    {
+       
+    }
+
+    public bool IsInUSA()
+    {
+        return true;
+    }
+
+}
