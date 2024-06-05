9dd7bc3ae8c84e33139ed32bd6c9d42a3c7389bf
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 20:48:47 +0100

    Encapsulation Project

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -5,12 +5,23 @@ class Customer
 
     public Customer(string Name, Address Address)
     {
-       
+        _name = Name;
+        _address = Address;
     }
 
     public bool IsInUSA()
     {
-        return true;
+        return _address.IsInUSA();
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public Address Address
+    {
+        get { return _address; }
     }
 
 }
