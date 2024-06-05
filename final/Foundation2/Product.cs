ccca4d27b84bee7074f23649196801b27b0919fa
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 15:35:30 +0100

    Create Product.cs
    Design Activity

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,20 @@
+class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    public Product(string name, string productId, Double price, int quantity)
+    {
+        
+    }
+
+    public double GetTotalCost()
+    {
+        return 1;
+    }
+
+   
+    
+}
