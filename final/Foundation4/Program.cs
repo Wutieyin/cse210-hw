0e3a53a98213c30b0324dbdf05d77901e2d047d7
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 21:05:14 +0100

    Polymorphism Project

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Activity[] activities = new Activity[] {new Running(new DateTime(2024, 10, 7), 30, 3.0), new Cycling(new DateTime(2024, 10, 7), 45, 20), new Swimming(new DateTime(2024, 10, 7), 60, 15)};
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine();
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine();
+        }   
     }
 }
