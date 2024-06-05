931a4b6eb02cf88e4db397386ad380908add873d
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 15:34:48 +0100

    Create Order.cs
    Design Activity

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,32 @@
+class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+       
+    }
+
+    public void AddProduct(Product product)
+    {
+       
+    }
+
+    public double CalculateTotalPrice()
+    {
+        
+        return 1;
+    }
+
+    public string GetPackingLabel()
+    {
+        return "";
+        
+    }
+
+    public string GetShippingLabel()
+    {
+        return "";
+    }
+}
