cb93ef99fa17204b72a2062f650fe4bff2d1146e
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 21:03:58 +0100

    Polymorphism Project

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -5,26 +5,26 @@ class Cycling : Activity
     public Cycling(DateTime date, int duration, double speed)
         : base(date, duration)
     {
-        
+        _speed = speed;
     }
 
     public override double GetSpeed()
     {
-        return 1;
+        return _speed;
     }
 
     public override double GetDistance()
     {
-        return 1;
+        return _speed * duration / 60;
     }
 
     public override double GetPace()
     {
-        return 1;
+        return 60 / _speed;
     }
 
     public override string GetSummary()
     {
-        return "";  
+        return $"{base.GetSummary()} - Speed: {_speed:F2} kph, Distance: {GetDistance():F2} km, Pace: {GetPace():F2} min/km";
     }
 }
