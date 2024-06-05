5508aaf16bb143d62ec2f65905b5faeb7e2a1c4c
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 15:59:30 +0100

    Create Address.cs
    Design Activity

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,17 @@
+class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        
+    }
+
+    public override string ToString()
+    {
+        return "";
+    }
+}
