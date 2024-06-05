205b06dbf416d0084c0159d23e0e1bce2dc28297
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 05:57:19 +0100

    Update Video.cs
    Final Project - Program 1: Abstraction Design

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,1 +1,28 @@
+class Video
+{
+    public string _title; 
+    public string _author; 
+    public int _length; 
+    private List<Comment> _comments;
+
+    public Video(string Title, string Author, int Length)
+    {
+      
+    }
+
+    public void AddComment(string NameOfCommenter, string Text)
+    {
+        
+    }
+
+    public int GetNumComments()
+    {
+        return _comments.Count;
+    }
+
+        public void DisplayInfo()
+    {
+        
+    }
+}
 
