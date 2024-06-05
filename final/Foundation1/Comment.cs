8ed705a109d8449dcd03cd427327120bc17d7471
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 20:42:12 +0100

    Abstraction Project

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -1,14 +1,24 @@
 class Comment
 {
+  
     private string _nameOfCommenter; 
     private string _text; 
 
    public Comment(string NameOfCommenter, string Text)
     {
-         
+         _nameOfCommenter = NameOfCommenter ; 
+         _text = Text;
     } 
 
+   
+ public string NameOfCommenter
+    {
+        get { return _nameOfCommenter; }
+    }
+
+    public string Text
+    {
+        get { return _text; }
+    }
 
 }
-
-
