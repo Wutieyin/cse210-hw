068f3d30858a04faed2e6d39e6541cf16474078e
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-06-05 20:51:20 +0100

    Encapsulation Project

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -7,14 +7,25 @@ class Product
 
     public Product(string name, string productId, Double price, int quantity)
     {
-        
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
     }
 
     public double GetTotalCost()
     {
-        return 1;
+        return _price * _quantity;
     }
 
-   
+     public string Name
+    {
+        get { return _name; }
+    }
+
+ public string ProductId
+    {
+        get { return _productId; }
+    }
     
 }
